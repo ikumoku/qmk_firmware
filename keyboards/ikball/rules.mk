8dcfd6607e87fe1222b9429271a19aa7022e5b7b
ikumoku <hisaoikuta@gmail.com>
2023-06-11 11:14:45 +0900

    add LED

--- keyboards/ikball/rules.mk
+++ keyboards/ikball/rules.mk
@@ -1,7 +1,14 @@
-# This file intentionally left blank
+# メモリー節約　
+LTO_ENABLE = yes #リンク時最適化
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+
 
 CONSOLE_ENABLE = yes
 
+# Optical sensor driver for trackball.
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
 SRC += 	paw3204.c
@@ -10,8 +17,13 @@ SRC += 	paw3204.c
 #OLED_DRIVER = SSD1306
 
 ENCODER_ENABLE = yes
-SPLIT_KEYBOARD = yes
 
+# Keyball39 is split keyboard.
+SPLIT_KEYBOARD = yes
 
 # atmega32u4 3.3v
 #F_CPU = 8000000
+
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = WS2812
+
