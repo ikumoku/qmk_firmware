c20e0c561c698684b6d57c7a33ef3e6f6ed0a6cd
ikumoku <hisaoikuta@gmail.com>
2023-06-18 15:16:17 +0900

    add LED

--- keyboards/ikball/keymaps/via/keymap.c
+++ keyboards/ikball/keymaps/via/keymap.c
@@ -139,11 +139,10 @@ void matrix_scan_user(void) {
             static char type_count_str[7];
             itoa(stat, type_count_str, 10);
             oled_set_cursor(0, 8);
-          //  oled_write_P(PSTR("Ball"), false);
-            // oled_write(type_count_str, false);
-            oled_set_cursor(0, 11);
+
+            oled_set_cursor(0, 13);
             oled_write(format_4d(mouse_rep.x), false);
-            oled_set_cursor(0, 14);
+            oled_set_cursor(0, 15);
             oled_write(format_4d(mouse_rep.y), false);
         }
 
@@ -288,41 +287,48 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             oled_set_cursor(0, 2);
             oled_write_ln_P(PSTR("--0--"), false);
             oled_set_cursor(0, 3);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 4);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 5);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             break;
         case _L1:
             dprint("layer 1\n");
             mouse_mode_scroll = false;
-            oled_write_ln_P(PSTR("     "), false);
+            oled_set_cursor(0, 2);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 3);
             oled_write_ln_P(PSTR("--1--"), false);
             oled_set_cursor(0, 4);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 5);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             break;
             break;
         case _L2:
             dprint("layer 2\n");
             mouse_mode_scroll = false;
             oled_set_cursor(0, 2);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 3);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             oled_set_cursor(0, 4);
             oled_write_ln_P(PSTR("--2--"), false);
             oled_set_cursor(0, 5);
-            oled_write_ln_P(PSTR("     "), false);
+            oled_write_ln_P(PSTR("-   -"), false);
             break;
         case _L3:
             dprint("layer 3\n");
             mouse_mode_scroll = true;
-            oled_set_cursor(2, 5);
-            oled_write_ln_P(PSTR("3"), false);
+            oled_set_cursor(0, 2);
+            oled_write_ln_P(PSTR("-   -"), false);
+            oled_set_cursor(0, 3);
+            oled_write_ln_P(PSTR("-   -"), false);
+            oled_set_cursor(0, 4);
+            oled_write_ln_P(PSTR("-   -"), false);
+            oled_set_cursor(0, 5);
+            oled_write_ln_P(PSTR("--3--"), false);
             break;
         case _L4:
             dprint("layer 4\n");
--- keyboards/ikball/rules.mk
+++ keyboards/ikball/rules.mk
@@ -1,10 +1,13 @@
 # メモリー節約　
 LTO_ENABLE = yes #リンク時最適化
 
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no        # Console for debug
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 COMMAND_ENABLE = no         # Commands for debug and configuration
 AUDIO_ENABLE = no           # Audio output
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionalit
+
 # This is unnecessary for processing KC_MS_BTN*.
 # MOUSEKEY_ENABLE = no
 
@@ -25,6 +28,17 @@ SPLIT_KEYBOARD = yes
 # atmega32u4 3.3v
 #F_CPU = 8000000
 
-#RGBLIGHT_ENABLE = yes
-#RGBLIGHT_DRIVER = WS2812
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = WS2812
+RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not work yet)
+RGB_MATRIX_DRIVER = WS2812
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+
+
+# Disable other features to squeeze firmware size
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+
 
