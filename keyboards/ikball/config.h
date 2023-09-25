5effd291bf5dc7014b0deb9f271107c62c189a9e
ikumoku <hisaoikuta@gmail.com>
2023-09-25 17:05:12 +0900

    Update config.h

--- keyboards/ikball/config.h
+++ keyboards/ikball/config.h
@@ -35,7 +35,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 // pin Assignments
-//      D3 TX   RGB_DI
+//      D3 TX   RGB_DI ←　　ROW4
 //      D2 RX   SERIAL
 //   GND
 //   GND
@@ -55,13 +55,16 @@
 
 // Key matrix parameters
 #define MATRIX_ROWS         4 * 2
+// #define MATRIX_ROWS         5 * 2
 #define MATRIX_COLS         6
+
 #define MATRIX_ROW_PINS     { F4, F5, F6, F7 }          //F4 enc push sw
+// #define MATRIX_ROW_PINS     { F4, F5, F6, F7 , D3}          //F4 enc push sw
 #define MATRIX_COL_PINS     { D4, C6, D7, E6, B4, B5 }  //B5 enc push sw only
 //#define MATRIX_MASKED
 //#define DEBOUNCE            5
 #define DIODE_DIRECTION     COL2ROW
-//#define DIODE_DIRECTION     ROW2COL
+//#define DIODE_DIRECTION     ROW2COLjkj
 
 #define SOFT_SERIAL_PIN D2 //Split Keyboard specific options
 //#define SELECT_SOFT_SERIAL_SPEED 5
